b3905175c9be3004bb565ca01d84c7e487e7d34e
Alexandre Kajihara <alexandre.ykz@gmail.com>
2021-02-22 10:59:55 -0300

    exemplo de acutest

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "util.h"
+
+int main(){
+    printf("%d\n", add(2, 2));
+    return 0;
+}
---
+++ unit_test.c
@@ -0,0 +1,11 @@
+#include "acutest.h"
+#include "util.h"
+
+void test_add(void){
+    TEST_CHECK_(add(2, 2) == (2 + 2), "add(%d, %d) == %d, 2, 2, (2 + 2)");
+}
+
+TEST_LIST = {
+    {"int add(int, int);", test_add},
+    {0}
+};
